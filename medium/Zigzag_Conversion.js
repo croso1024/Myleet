4e50045c2ebc4898e98fe1a3707614759a757dc5
croso1024 <croso1024@gmail.com>
2022-07-11 20:08:13 +0800

    0710

---
+++ medium/Zigzag_Conversion.js
@@ -0,0 +1,11 @@
+var convert = function(s, numRows) {
+    let array = new Array();
+    for(let i=0 ;i<numRows ; i+=1)
+        {
+            array[i] = new Array();
+        }
+    for(let char of s)
+        {
+            
+        }
+};
